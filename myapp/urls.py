77e1d59144191aeca7aa5cf23dfa00e92d058dc7
1galeforce <148296534+1galeforce@users.noreply.github.com>
2024-03-10 02:35:25 +0000

    building more pages with images and hyperlinks

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from . import views
 from .views import HomePageView
+from .views import AboutView
+from .views import SurfingView
+from .views import LocationView
+from .views import WipeoutView
+from .views import DangerView
+
 
 urlpatterns = [
   path("", HomePageView.as_view(), name="home"),
-  path("class/",views.ClassView.as_view(), name='class_view'),
+  path("about/", AboutView.as_view(), name="about"),
+  path('surfing/', SurfingView.as_view(), name='bigwavesurfing'),
+  path('location/', LocationView.as_view(), name='location_view'),
+  path('wipeout/', WipeoutView.as_view(), name= 'wipeout_view'),
+  path('danger/', DangerView.as_view(), name='danger_view'),
+  path('class/', views.ClassView.as_view(), name='class_view'),
   path('function/', views.function_view, name='function_view'),
-  path('class/', views.ClassView.as_view(), name='bigwavesurfing'),
-  path('class/', views.ClassView.as_view(), name='locations'),
+  
 ]
